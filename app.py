726c2da187e25b6be907f96277bce0d4436479c9
ap7t <adampurcell7t@gmail.com>
2020-09-20 19:01:34 +0100

    added bar chart

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import dash_core_components as dcc
 from dash.dependencies import Input, Output, State
 
 import pandas as pd
-from datetime import datetime as dt
+from collections import Counter
+import plotly.express as px
+from datetime import datetime as dt, date, timedelta
 import os
 from spotipy import Spotify
 import spotipy.util as util
@@ -19,29 +21,36 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 username = os.getenv("SPOTIFY_USER_ID")
 SCOPE = "user-library-read"
-LIMIT = 50
 
 token = util.prompt_for_user_token(username, SCOPE)
 sp = Spotify(token)
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, title="Dash App")
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, title="Reminiscify")
 
 r = sp.current_user_saved_tracks()
-d = get_liked_songs(sp)
-df = pd.DataFrame.from_dict(d)
-df1 = df.iloc[:, 3:len(df.columns)]
+df = get_liked_songs(sp)
+
+# df = pd.read_csv("liked_songs.csv")
+df["Added At"] = pd.to_datetime(df["Added At"])
+df["Day Num"] = df["Added At"].dt.weekday
+df["Day"] = df["Added At"].dt.day_name()
+
+days_df = df.groupby(["Day Num", "Day"]).size().reset_index(name="Songs Saved")
+
+fig = px.bar(days_df, x="Day", y="Songs Saved")
 
 # ------------------------------------------------------------------------
 # Layout
 # ------------------------------------------------------------------------
 app.layout = html.Div([
         html.Div(id="hidden-div", style={"display":"none"}),
-        html.H1("Spotify Liked Songs", style={"color":"#1DB954"}),
+        html.H1("Reminiscify", style={"color":"#1DB954"}),
+
         dcc.DatePickerRange(
             id="my-date-picker-range",
-            start_date = dt(2020, 8, 1),
-            end_date = dt.today(),
+            start_date = date(2020, 9, 1),
+            end_date =date.today(),
             min_date_allowed = dt(2015, 9, 22),
-            max_date_allowed = dt.today(),
+            max_date_allowed = date.today(),
             display_format="DD/MM/YYYY"
         ),
         html.Div([
@@ -56,7 +65,7 @@ app.layout = html.Div([
             html.Div([
                 html.H6(id="num-albums"), 
                 html.P(children="Different Albums")
-                ]),
+                ])
         ],
         id="figures-div"),
         html.Div([
@@ -68,6 +77,10 @@ app.layout = html.Div([
             )],
             id="playlist-div"), 
             html.Button("Create Playlist", id="button"),
+        dcc.Graph(
+        id="days-graph",
+        figure=fig
+        ),
         html.Div(id="my-table", 
                 style={
                 "width":"50%",
@@ -85,14 +98,45 @@ app.layout = html.Div([
         [Output("my-table", "children"),
         Output("num-tracks", "children"),
         Output("num-artists", "children"),
-        Output("num-albums", "children")],
+        Output("num-albums", "children"),
+        Output('days-graph', 'figure')],
         [Input("my-date-picker-range", "start_date"),
         Input("my-date-picker-range", "end_date")]
 )  
 def update_info(start_date, end_date):
     dff = filter_songs(df, start_date, end_date)
-    counts = dff.explode("Artist").nunique()
-    return html.Div([generate_table(dff.iloc[:, 3:len(df.columns)])]), counts["Uri"], counts["Artist ID"], counts["Album ID"]
+    dff["Added At"] = dff["Added At"].dt.strftime("%c") # nice readable datetime format (done after filter_songs func as need as datetime for that func)
+    
+    # if no songs saved on a day without following code the bar chart only shows days with things saved
+    # update the dataframe to include an entry for all days so the graph always has every day on the x axis
+    week_days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    days_df = dff.groupby(["Day Num", "Day"]).size().reset_index(name="Songs Saved")
+    for i, day in enumerate(week_days):
+        if day not in list(days_df["Day"]):
+            days_df.loc[len(days_df)] = [i, day, 0]
+
+    start_dt= dt.strptime(start_date, "%Y-%m-%d")
+    end_dt =   dt.strptime(end_date, "%Y-%m-%d")
+    start_weekday = int(start_dt.weekday())
+    end_weekday = int(end_dt.weekday())
+    
+    # remove entries for days that don't exist between selected date range if range is in the same week 
+    days_between = end_dt - start_dt
+    if days_between.days < 7:
+        days_df = days_df[days_df["Day Num"].isin([i for i in range(start_weekday, end_weekday + 1)])]
+    
+    # the face im using datetime for the end date fucking me up nmust fix tomorrow
+
+    days_df.sort_values(by=["Day Num"], inplace=True)
+    days_df.drop(labels=["Day Num"], axis=1, inplace=True)
+
+    fig = px.bar(days_df, x="Day", y="Songs Saved")
+    fig.update_layout(transition_duration=250)
+    
+    counts = dff.explode("Artist").nunique() # come back to artists 
+
+    dff = dff.drop(labels=["Uri", "Artist ID", "Album ID", "Day", "Day Num"], axis=1)
+    return html.Div([generate_table(dff)]), counts["Uri"], counts["Artist ID"], counts["Album ID"], fig
 
 @app.callback(
     Output("hidden-div", "children"),
@@ -115,4 +159,4 @@ def create_playlist(n_clicks, playlist_name, start_date, end_date):
     return 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True) 
--- helpers.py
+++ helpers.py
@@ -1,11 +1,15 @@
 import dash_html_components as html
-from datetime import datetime as dt
+import pandas as pd
 
-def get_liked_songs(sp, limit=50, total=250):
+def get_liked_songs(sp, limit=50):
     offset = 0
     response = sp.current_user_saved_tracks(limit=limit, offset=offset)
     total = response['total']
-    d = {"Uri":[], "Artist ID": [], "Album ID":[], "Artwork": [], "Track": [],"Artist": [],  "Album": [], "Added At": []}
+    # total = 500
+    d = {"Uri":[], "Artist ID": [], "Album ID":[], "Artwork": [], "Track": [],
+        "Artist": [], "Album": [], "Added At": []}
+
+    # do label with album stuff
 
     while offset < total:
         try:
@@ -15,21 +19,55 @@ def get_liked_songs(sp, limit=50, total=250):
             d["Artist"] += [response["items"][i]["track"]["artists"][0]["name"] for i in range(limit)]
             d["Artist ID"] += [response["items"][i]["track"]["artists"][0]["id"] for i in range(limit)]
             d["Album"] += [response["items"][i]["track"]["album"]["name"] for i in range(limit)]
-            d["Album ID"] += [response["items"][i]["track"]["album"]["id"] for i in range(limit)]
-            d["Added At"] += [dt.strptime(response["items"][i]["added_at"], "%Y-%m-%dT%XZ") for i in range(limit)] # use pd datetime
+            d["Album ID"] += [response["items"][i]["track"]["album"]["id"] for i in range(limit)] # do it this way so can be used in album endpoint
+            d["Added At"] += [response["items"][i]["added_at"] for i in range(limit)] # use pd datetime
         except IndexError:
             pass
 
         offset += limit
         response = sp.current_user_saved_tracks(limit=limit, offset=offset)
+    
+    df = pd.DataFrame.from_dict(d)
+    df["Added At"] = pd.to_datetime(df["Added At"])
+    return df
 
-    return d
+def get_saved_albums(sp, limit=20):
+    offset = 0
+    response = sp.current_user_saved_albums(limit=limit, offset=offset)
+    total = response['total']
+    total = 500
+    d = {"Artist ID": [], "Album ID":[], "Artwork": [], "Artist": [], "Album": [],"Label": [], "Added At": []}
+
+    # do label with album stuff
+
+    while offset < total:
+        try:
+            d["Artwork"] += [response["items"][i]["album"]["images"][2]["url"] for i in range(limit)]
+            d["Artist"] += [response["items"][i]["album"]["artists"][0]["name"] for i in range(limit)]
+            d["Artist ID"] += [response["items"][i]["album"]["artists"][0]["id"] for i in range(limit)]
+            d["Album"] += [response["items"][i]["album"]["name"] for i in range(limit)]
+            d["Album ID"] += [response["items"][i]["album"]["id"] for i in range(limit)] # do it this way so can be used in album endpoint
+            d["Label"] += [response["items"][i]["album"]["label"] for i in range(limit)]
+            d["Added At"] += [response["items"][i]["added_at"] for i in range(limit)] # use pd datetime
+        except IndexError:
+            pass
+
+        offset += limit
+        response = sp.current_user_saved_albums(limit=limit, offset=offset)
+    
+    
+    df = pd.DataFrame.from_dict(d)
+    df["Added At"] = pd.to_datetime(df["Added At"])
+    return df
 
 def filter_songs(df, start_date, end_date):
-    dff = df[(start_date < df['Added At']) & (df['Added At'] < end_date)]
+    dff = df[(start_date <= df['Added At']) & (df['Added At'] <= end_date)]
     return dff
 
+
+
 def generate_table(dataframe, max_rows=25):
+    # dataframe["Added"]
     return html.Table([
             html.Thead(
                 html.Tr([html.Th(col) for col in dataframe.columns])
